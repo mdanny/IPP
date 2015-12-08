a9eda7c2b66ecada23c355e4bfa7e8efd641582b
Daniel Macrinici <macrinici.d@gmail.com>
2015-12-08 23:12:44 +0200

    added primitive registration using SQLAlchemy

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,45 @@
 from flask import Flask
+from flask import session, request
+from flask import render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates')
+app.debug = True
+app.secret_key = 'secret'
+app.config.update({
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+    })
+db = SQLAlchemy(app)
 
-@app.route("/")
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(40), unique = True)
 
-def main():
-    return "Welcome!"
+def current_user():
+    if 'id' in session:
+        uid = session['id']
+        return User.query.get(uid)
+    return None
+
+@app.route('/', methods = ('GET', 'POST'))
+
+def home():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        user = User.query.filter_by(username = username).first()
+        if not user:
+            user = User(username = username)
+            db.session.add(user)
+            db.session.commit()
+        session['id'] = user.id
+        return redirect('/')
+    user = current_user()
+    return render_template('home.html', user = user)
+
+# def main():
+#     return "Welcome!"
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()

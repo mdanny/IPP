573a123f3bde503cea49bb4b83b46654870c2b2d
Daniel Macrinici <macrinici.d@gmail.com>
2015-12-08 23:48:08 +0200

    added the client, which represents our application

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import session, request
 from flask import render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import gen_salt
+from flask import jsonify
 
 app = Flask(__name__, template_folder = 'templates')
 app.debug = True
@@ -11,9 +13,37 @@ app.config.update({
     })
 db = SQLAlchemy(app)
 
+#The User table in the db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(40), unique = True)
+
+#The Client table in the db
+class Client(db.Model):
+    client_key = db.Column(db.String(40), primary_key = True)
+    client_secret = db.Column(db.String(55), index = True, nullable =   False)
+
+    # creator of the client
+    user_id = db.Column(db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    _realms = db.Column(db.Text)
+    _redirect_uris = db.Column(db.Text)
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_realms(self):
+        if self._realms:
+            return self._realms.split()
+        return []
 
 def current_user():
     if 'id' in session:
@@ -36,9 +66,24 @@ def home():
     user = current_user()
     return render_template('home.html', user = user)
 
-# def main():
-#     return "Welcome!"
-
+#Added another route for the client. If the client (which is some app for instance) passes
+#the authentication, then it is given a client_key
+@app.route('/client')
+def client():
+    user = current_user()
+    if not user:
+        return redirect('/')
+    item = Client(
+        client_key = gen_salt(40),
+        client_secret = gen_salt(50),
+        user_id = user.id,
+    )
+    db.session.add(item)
+    db.session.commit()
+    return jsonify(
+        client_key = item.client_key,
+        client_secret = item.client_secret
+    )
 
 if __name__ == "__main__":
     db.create_all()
